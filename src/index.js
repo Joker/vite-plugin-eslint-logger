c3cc236bb446b9a5751f117a8a8dabaec17897b2
Joker <nice.way@live.com>
2021-11-07 21:54:34 +0300

    mjs >> cjs

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import { normalizePath } from 'vite'
+const { normalizePath } = require('vite')
+const { createFilter } = require('@rollup/pluginutils')
 
-const eslint = () => {
+module.exports = function eslint(options = {}) {
+	const { includeFiles, excludeFiles } = options
+	const target = createFilter(includeFiles || ['**/*.js', '**/*.mjs', '**/*.cjs', '**/*.jsx', '**/*.ts', '**/*.tsx'], excludeFiles || /node_modules/)
+
 	return {
 		name: 'vite:eslint-logger',
 		apply: 'serve',
 		async transform(code, id) {
 			const path = normalizePath(id)
-			console.log(path)
+			if (target(path)) console.log('transform ---', path)
 		},
 		async handleHotUpdate({ file }) {
 			const path = normalizePath(file)
-			console.log(path)
+			if (target(path)) console.log('handleHotUpdate ---', path)
 		},
 	}
 }
-
-export default eslint
